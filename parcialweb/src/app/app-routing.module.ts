49b5bdd4b4c2850fc8865a9b719d7ef1a0aea8bc
Camelo21 <andresmelo14@outlook.es>
2023-05-25 20:46:11 -0500

    final

---
+++ parcialweb/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LibroComponent } from './libro/libro.component';
+const routes: Routes = [
+{path: 'libro', component:LibroComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
